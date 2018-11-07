fda2944511e002dc623c7d5596b3146d6084e9bd
Jean Cavallo <jean.cavallo@hotmail.fr>
2018-11-07 14:18:23 +0100

    Truncate addresses

--- truncate.py
+++ truncate.py
@@ -1,6 +1,15 @@
 from lxml import etree
 
-countries = ('fr', 'be', 'it', 'mc', 'es', 'ch', 'gb', 'us')
+countries = (
+    'address_format_fr',
+    'address_format_be',
+    'address_format_it',
+    'address_format_mc',
+    'address_format_es',
+    'address_format_ch',
+    'address_format_gb',
+    'address_format_us',
+    )
 
 
 def truncate_records():
@@ -8,12 +17,12 @@ def truncate_records():
     with open('address.xml', 'r') as f:
         tree = etree.parse(f)
     for element in tree.xpath("//record[@model='party.address.format']"):
-        if element.attrib['id'][-2:] not in countries:
+        if element.attrib['id'] not in countries:
             to_remove.append(element)
     for element in to_remove:
         element.getparent().remove(element)
-    with open('address.xml', 'w') as f:
-        f.write('<?xml version="1.0"?>\n')
+    with open('address.xml', 'wb') as f:
+        f.write(b'<?xml version="1.0"?>\n')
         tree.write(f, pretty_print=True, encoding='utf-8')
 
 
